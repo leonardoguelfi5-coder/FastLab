cbd6d5e333b835e21d6dd8e4e95c4d527f93ec59
leonardoguelfi5-coder <leonardoguelfi5@gmail.com>
2026-05-13 14:32:55 +0200

    feat: add /scegli, /passo/:tipo, /modifica/:id routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ListaRilevazioni } from './pages/ListaRilevazioni'
 import { NuovaRilevazione } from './pages/NuovaRilevazione'
+import { SceltaMisurazione } from './pages/SceltaMisurazione'
+import { PassoPianta } from './pages/PassoPianta'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ListaRilevazioni />} />
+        <Route path="/scegli" element={<SceltaMisurazione />} />
+        <Route path="/passo/:tipo" element={<PassoPianta />} />
         <Route path="/nuova" element={<NuovaRilevazione />} />
+        <Route path="/modifica/:id" element={<NuovaRilevazione />} />
       </Routes>
     </BrowserRouter>
   )
